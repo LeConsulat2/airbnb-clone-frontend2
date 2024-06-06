44078a15725207ba3c5087dbeb43c100d8c4f130
LeConsulat2 <wooine2@gmail.com>
2024-06-07 00:00:39 +1200

    Update NotFound.tsx

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,11 +1,13 @@
-import { VStack, Heading, Text, Button } from "@chakra-ui/react";
+import { VStack, Heading, Text, Button, Link } from "@chakra-ui/react";
 
 export default function NotFound() {
   return (
-    <VStack>
+    <VStack bg="gray.100" justifyContent={"center"} minHeight="100vh">
       <Heading>Page Not Found.</Heading>
       <Text>It seems that you are lost.</Text>
-      <Button>Go Home</Button>
+      <Link>
+        <Button colorScheme={"twitter"} variant={"link"}>Go Home &rarr;</Button>
+      </Link>
     </VStack>
   );
 }
