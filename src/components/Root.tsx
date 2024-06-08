3125deddff79331adc6b942cadc12c96904073e4
LeConsulat2 <wooine2@gmail.com>
2024-06-08 23:02:16 +1200

    19.2 Sign up Modal

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,12 +1,83 @@
-// components/Root.tsx
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  VStack,
+  useDisclosure,
+} from '@chakra-ui/react';
 import React from 'react';
+import { FaAirbnb, FaLock, FaMoon, FaUserNinja } from 'react-icons/fa6';
 import { Outlet } from 'react-router-dom';
 
 export default function Root() {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+
   return (
-    <div>
-      <h1>I'm root</h1>
+    <Box>
+      <HStack
+        justifyContent="space-between"
+        py="5"
+        px="10"
+        borderBottomWidth={1}
+      >
+        <Box color="red.500">
+          <FaAirbnb size="40" />
+        </Box>
+        <HStack spacing="2">
+          <IconButton
+            variant="ghost"
+            aria-label="Toggle dark mode"
+            icon={<FaMoon />}
+          />
+          <Button onClick={onOpen}>Log in</Button>
+          <Button colorScheme="red">Sign up</Button>
+        </HStack>
+      </HStack>
+      <Modal motionPreset="slideInBottom" onClose={onClose} isOpen={isOpen}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Log in</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <VStack spacing={4}>
+              <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                  <Box color="gray.500">
+                    <FaUserNinja />
+                  </Box>
+                </InputLeftElement>
+                <Input variant="filled" placeholder="Username" />
+              </InputGroup>
+              <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                  <Box color="gray.500">
+                    <FaLock />
+                  </Box>
+                </InputLeftElement>
+                <Input
+                  variant="filled"
+                  placeholder="Password"
+                  type="password"
+                />
+              </InputGroup>
+              <Button mt={4} colorScheme="red" w="100%">
+                Log in
+              </Button>
+            </VStack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
       <Outlet />
-    </div>
+    </Box>
   );
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,8 @@
+import { Box } from '@chakra-ui/react';
 import React from 'react';
 
 const Home: React.FC = () => {
-  return <div>Home Page</div>;
+  return <Box ml={"5"}>Home Page</Box>;
 };
 
 export default Home;
